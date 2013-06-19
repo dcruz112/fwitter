44f9f4d5760054e915ae9de52cabad7305208368
AndrewGoldstein <goldstein.andrew.s@gmail.com>
2013-06-19 14:27:33 +0000

    added validations to limit the twitter handles that a person could provide, added error message, changed biography limit to 160 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
 	has_many :tweets
 
+
+
+	validates :handle, uniqueness: { case_sensitive: false }
+	validates :handle, format: { with: /\A[@][a-zA-Z0-9_]+\z/,
+    message: "Handle should start with '@' and conclude with only letters, numbers, and '_'" }
+
+
+
 	def full_name
 		first_name + " " + last_name
 	end
