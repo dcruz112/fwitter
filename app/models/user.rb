60bd5db2db738ad5b2fb7c0560a496fab26b6525
AndrewGoldstein <goldstein.andrew.s@gmail.com>
2013-06-19 14:44:08 +0000

    added new handle_at method in the models/user.rb file to allow user to enter handle and then the user interface to display @handle

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,18 @@ class User < ActiveRecord::Base
 
 
 	validates :handle, uniqueness: { case_sensitive: false }
-	validates :handle, format: { with: /\A[@][a-zA-Z0-9_]+\z/,
-    message: "Handle should start with '@' and conclude with only letters, numbers, and '_'" }
+	# validates :handle, format: { with: /\A[@][a-zA-Z0-9_]+\z/,
+ 	# message: "Handle should start with '@' and conclude with only letters, numbers, and '_'" }
+	
+	validates :handle, format: { with: /\A[a-zA-Z0-9_]+\z/,
+    message: "Only use letters, numbers, and '_'" }
 
 
 
+	def handle_at
+		"@" + handle
+	end
+	
 	def full_name
 		first_name + " " + last_name
 	end
