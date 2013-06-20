71da19d2a54837af13277c9ec697af4769598a6f
David Cruz <dcruz112@gmail.com>
2013-06-20 10:12:26 -0400

    Adds the e-mail automatically

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -83,7 +83,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :handle, :biography, :current_location)
+      params.require(:user).permit(:first_name, :last_name, :handle, :biography, :current_location, :email)
     end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,21 @@ require 'net/ldap'
 class User < ActiveRecord::Base
 	has_many :tweets
 
-    def search_ldap(login)
+	def search_ldap(login)
 	    ldap = Net::LDAP.new(:host => "directory.yale.edu", :port => 389)
 	    filter = Net::LDAP::Filter.eq("uid", login)
 	    attrs = ["givenname", "sn", "eduPersonNickname", "telephoneNumber", "uid",
 	             "mail", "collegename", "curriculumshortname", "college", "class"]
 	    result = ldap.search(:base => "ou=People,o=yale.edu", :filter => filter, :attributes => attrs)
-	    if !result.empty?
+		if !result.empty?
 	    	@nickname = result[0][:eduPersonNickname]
-	    	if @nickname.empty?
+			if @nickname.empty?
 				self.first_name  = result[0][:givenname][0]
 			else
 			    self.first_name  = @nickname[0]
 			end
 			self.last_name   = result[0][:sn][0]
+			self.email   = result[0][:mail][0]
 		end
 	end
 
