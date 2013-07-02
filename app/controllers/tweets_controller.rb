521f335bf78557a2feb077e51183fc24a9604265
AndrewGoldstein <goldstein.andrew.s@gmail.com>
2013-07-02 14:10:59 +0000

    minor changes to user page feed structure

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -122,8 +122,7 @@ class TweetsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_tweet
-      @tweet = Tweet.find(params[:id])
-      
+      @tweet = Tweet.find(params[:id])    
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
