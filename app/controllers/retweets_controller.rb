82a6fccd60694d029561238231f3caab8c648b58
AndrewGoldstein <goldstein.andrew.s@gmail.com>
2013-07-02 15:47:35 +0000

    restructuring navbar

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -13,7 +13,8 @@ class RetweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
     @retweet = Retweet.new(content: @tweet.content, user_id: @tweet.user_id,  poster_id: current_user.id, tweet_id: @tweet.id )
     @retweet.save
-    render @retweet
+    redirect_to root_path
+    #render @retweet
   end
 
   # GET /retweets/new
